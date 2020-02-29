d24d84f0fd847d7784d26b0946805cc9d9a735fb
Andrew Caird <acaird@gmail.com>
2020-02-28 22:06:03 -0500

    took out defauls, added argparse

--- pdfsentiment.py
+++ pdfsentiment.py
@@ -1,17 +1,21 @@
 #!/usr/bin/env python3
 
+import argparse
 from PdfDocument.PdfDocument import PdfDocument
+
+
+def process_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-f", "--file", help="file or files to process", nargs="+")
+
+    return parser.parse_args()
+
 
 if __name__ == "__main__":
 
-    pdffiles = [
-        "examples/tb125heff-newusers.pdf",
-        "examples/tb125menke-tug19.pdf",
-        "examples/tb125reutenauer-xetex.pdf",
-        "examples/tb125tug19-agm.pdf",
-    ]
+    args = process_args()
 
-    for pdffile in pdffiles:
+    for pdffile in args.file:
         pdf = PdfDocument(pdffile)
         print(pdffile, pdf.creation_date)
 
